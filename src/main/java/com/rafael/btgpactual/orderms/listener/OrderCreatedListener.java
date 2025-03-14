49d612065ab338458503c6e594f0d167f23156fd
Rafael dos Santos Batista <rafaeldsantos442@gmail.com>
2025-03-14 01:57:29 -0300

    feat: Implementado o listener da fila do rabbitmq

---
+++ src/main/java/com/rafael/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,22 @@
+package com.rafael.btgpactual.orderms.listener;
+
+import com.rafael.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import static com.rafael.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+        logger.info("message consumed: {}", message);
+
+    }
+}
---
+++ src/main/java/com/rafael/btgpactual/orderms/listener/dto/OrderItemEvent.java
@@ -0,0 +1,6 @@
+package com.rafael.btgpactual.orderms.listener.dto;
+
+import java.math.BigDecimal;
+
+public record OrderItemEvent(String produto, Integer quantidade, BigDecimal preco) {
+}
