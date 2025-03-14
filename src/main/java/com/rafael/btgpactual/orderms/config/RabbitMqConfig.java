725721c2b6678cb71bf9cafc00576d3f3af1d254
Rafael dos Santos Batista <rafaeldsantos442@gmail.com>
2025-03-14 01:41:30 -0300

    feat: realizado mapeamento das entidades do banco

---
+++ src/main/java/com/rafael/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,8 @@
+package com.rafael.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
